05152ece341b0a313f12d95eaced366d81d26efd
Oliver <obryniarski@berkeley.com>
2018-08-28 01:44:11 -0700

    improved graph visuals and added a save for each polynomial created

--- polynomial.py
+++ polynomial.py
@@ -26,7 +26,7 @@ def fit_polynomial(data_list):
     return np.linalg.solve(A, b)
 
 
-data_one = [[5, 2], [2, 3], [3, 3], [1, 1]]
+data_one = [[1, 1], [2, 1.8], [3, 3.6],[4,4],[5,5],[6,6],[7,7],[8,8.12]]
 
 polynomial_coefficients_one = fit_polynomial(data_one)
 
@@ -36,13 +36,19 @@ def graph_polynomial(data_list, polynomial):
     x_max = max(point[0] for point in data_list)
     x = np.linspace(1 * x_min, 1 * x_max, 30)
     poly_x = [sum(polynomial[j] * (i ** j) for j in range(len(polynomial))) for i in x]
-
     poly_as_str = '+'.join(['%s$x^%i$' % (str(round(polynomial[i], 2)), i) for i in range(len(polynomial))])
-
-    plt.plot(x, poly_x, label='Polynomial: ' + poly_as_str)
+    print(poly_as_str)
+    with open('latest_polynomial_description.txt', 'w') as file:
+        file.write('Polynomial Coefficients: ' + str([round(polynomial[i], 2) for i in range(len(polynomial))]) + '\n')
+        file.write('Polynomial Degree: ' + str(len(polynomial) - 1) + '\n')
+        file.write('Fitted Points: ' + str(data_list)[1:-1])
+    plt.plot(x, poly_x, label='Polynomial: ' + poly_as_str if len(polynomial) < 4 else 'Polynomial: too long to display')
+    plt.title('Degree %i Polynomial, fitting %i data points' % (len(polynomial) - 1, len(data_list)))
     plt.legend()
     for point in data_list:
         plt.plot(point[0], point[1], marker='o')
+
+    plt.savefig('latest_polynomial_graph.png', dpi=72)
     plt.show()
 
 
