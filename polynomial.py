4e22802e5dd1efc9959bd337e1dc23ef804533a1
Oliver <obryniarski@berkeley.com>
2018-08-27 22:34:25 -0700

    first commit

---
+++ polynomial.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+def polynomial(n, degree=1):
+
+    return [n ** i for i in range(degree + 1)]
+
+
+def data_points_to_matrices(data_list):
+    A = []
+    b = []
+    for data_point in data_list:
+        A.append(polynomial(data_point[0], 2))
+        b.append(data_point[1])
+
+    return [A, b]
+
+
+data = [[1, 2], [2, 2], [3, 3]]
+matrices = data_points_to_matrices(data)
+A = matrices[0]
+b = matrices[1]
+
+print(A, '\n', b)
+
+S = np.linalg.solve(A, b)
+
+print(S)
